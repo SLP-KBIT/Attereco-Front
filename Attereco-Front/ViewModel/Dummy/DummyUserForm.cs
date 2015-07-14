a29f8e6a1745d4633c23078a0cf04b6e6bbcc47c
KentoTSUJI <s12t241@stmail.eng.kagawa-u.ac.jp>
2015-07-15 01:00:53 +0900

    [_VVM] create: DummyUserForm

---
+++ Attereco-Front/ViewModel/Dummy/DummyUserForm.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Attereco_Front.ViewModel.Dummy
+{
+    public class DummyUserForm : ViewModelBase
+    {
+        public DummyUserForm()
+        {
+        }
+    }
+}
---
+++ Attereco-Front/Views/Dummy/DummyUserForm.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Attereco_Front.Views.Dummy
+{
+    /// <summary>
+    /// Description for DummyUserForm.
+    /// </summary>
+    public partial class DummyUserForm : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the DummyUserForm class.
+        /// </summary>
+        public DummyUserForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
