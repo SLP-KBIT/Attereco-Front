4f8f29b0f2cceea651f4a804ed60843e50c0c2b3
KentoTSUJI <s12t241@stmail.eng.kagawa-u.ac.jp>
2015-07-16 00:43:54 +0900

    [M___] edit models

---
+++ Attereco-Front/Model/Felica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attereco_Front.Model
+{
+    /// <summary>
+    /// フェリカ周りのクラス
+    /// </summary>
+    public class Felica
+    {
+        public Felica(String _IDm)
+        {
+            this.IDm = _IDm;
+        }
+
+        /// <summary>
+        /// IDm
+        /// </summary>
+        public String IDm { get; private set; }
+    }
+}
--- Attereco-Front/Model/User.cs
+++ Attereco-Front/Model/User.cs
@@ -9,35 +9,40 @@ namespace Attereco_Front.Model
 {
     public class User
     {
-        public User(string name)
-        {
-            this.Name = name;
-            this.LoginTime = DateTime.Now;
-        }
-
         /// <summary>
         /// Userコンストラクタ
         /// </summary>
         /// <param name="name">name</param>
-        /// <param name="dynamic">new DynamicJson()</param>
-        public User(string name, dynamic dynamic)
+        /// <param name="dynamic">Postで獲得したJson</param>
+        public User(dynamic dynamic)
         {
-            this.Name = name;
-            this.LoginTime = DateTime.Now;
-            this.SetJson(this, dynamic);
+            this.Name = dynamic["User"];
+            this.LoginTime = dynamic["LoginTime"];
+            this.Project = dynamic["Project"];
+            this.Lank = dynamic["Lank"];
+            this.Json = dynamic.ToString();
         }
 
+        /// <summary>
+        /// 名前
+        /// </summary>
         public String Name { get; private set; }
 
+        /// <summary>
+        /// ログイン時間
+        /// </summary>
         public DateTime LoginTime { get; private set; }
+        
+        /// <summary>
+        /// 所属プロジェクト
+        /// </summary>
+        public String Project { get; private set; }
+
+        /// <summary>
+        /// 学年
+        /// </summary>
+        public int Lank { get; private set; }
 
         public String Json { get; private set; }
-
-        private void SetJson(User user, dynamic dynamic)
-        {
-            dynamic["User"] = this.Name;
-            dynamic["LoginTime"] = this.LoginTime;
-            this.Json = dynamic.toString();
-        }
     }
 }
