bcdf119562ba1be7fa07868048d08218eeb58ea7
KentoTSUJI <s12t241@stmail.eng.kagawa-u.ac.jp>
2015-07-17 23:55:39 +0900

    close #19 create: ユーザーモデル

--- Attereco-Front/Model/User.cs
+++ Attereco-Front/Model/User.cs
@@ -9,40 +9,81 @@ namespace Attereco_Front.Model
 {
     public class User
     {
+        #region コンストラクタ
         /// <summary>
         /// Userコンストラクタ
         /// </summary>
-        /// <param name="name">name</param>
-        /// <param name="dynamic">Postで獲得したJson</param>
+        /// <param name="name">名前</param>
+        /// <param name="sid">学籍番号</param>
+        public User(string name, string sid)
+        {
+            this.Name = name;
+            this.Sid = sid;
+            this.LoginTime = DateTime.Now;
+            this.Json = this.SetJson();
+        }
+
+        /// <summary>
+        /// Userコンストラクタ
+        /// </summary>
+        /// <param name="dynamic">Json</param>
         public User(dynamic dynamic)
         {
             this.Name = dynamic["User"];
+            this.Sid = dynamic["Sid"];
             this.LoginTime = dynamic["LoginTime"];
-            this.Project = dynamic["Project"];
-            this.Lank = dynamic["Lank"];
             this.Json = dynamic.ToString();
         }
+        #endregion
+
+        #region フィールド
+        /// <summary>
+        /// Idm
+        /// </summary>
+        private string Idm { get; set; }
 
         /// <summary>
         /// 名前
         /// </summary>
-        public String Name { get; private set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 学籍番号
+        /// </summary>
+        public string Sid { get; set; }
 
         /// <summary>
         /// ログイン時間
         /// </summary>
-        public DateTime LoginTime { get; private set; }
+        public DateTime LoginTime { get; set; }
         
         /// <summary>
         /// 所属プロジェクト
         /// </summary>
-        public String Project { get; private set; }
+        public string Project { get; set; }
 
         /// <summary>
         /// 学年
         /// </summary>
-        public int Lank { get; private set; }
+        public int Rank { get; set; }
 
-        public String Json { get; private set; }
+        /// <summary>
+        /// Json
+        /// </summary>
+        public string Json { get; set; }
+        #endregion
+
+        /// <summary>
+        /// SetJson
+        /// </summary>
+        /// <returns>json</returns>
+        private string SetJson()
+        {
+            dynamic json = new DynamicJson();
+            json["Name"] = this.Name;
+            json["Sid"] = this.Sid;
+            json["LoginTime"] = this.LoginTime;
+            return json.ToString();
+        }
     }
 }
