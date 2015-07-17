6ec8847a04530a68062be3d79e5a1ccf70cc8477
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-17 18:04:01 +0900

    refs #3 MVVMLightToolKitなしでのNUnitのテスト追加

---
+++ Attereco-Front-Test/MainViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Attereco_Front_Test
+{
+    /// <summary>
+    /// MainViewModelのテスト
+    /// </summary>
+    [TestFixture]
+    public class MainViewModelTest
+    {
+        
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void サンプル()
+        {
+            Assert.AreEqual(1 + 1, 2);
+        }
+    }
+}
