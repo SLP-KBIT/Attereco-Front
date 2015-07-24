01443fcef30ef68e738771d9ae55bd0c8dcdbb37
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-25 00:26:53 +0900

    [add] refs #5 実際のAPIによってリクエストを送る

--- Attereco-Front/Model/Common/AtterecoClient.cs
+++ Attereco-Front/Model/Common/AtterecoClient.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Attereco_Front.Model;
+using Attereco_Front.Model.Common;
 
 namespace Attereco_Front.Model.Common
 {
     public class AtterecoClient : IClient
     {
+        private HttpClient httpClient;
+
+        private Settings settings;
+
+        public AtterecoClient()
+        {
+            httpClient = new HttpClient();
+            settings = XMLFileManager.ReadXml<Settings>("Settings.xml");
+        }
+
         public User PostUser(User user)
         {
-            throw new NotImplementedException();
+            Dictionary<string, string> form = new Dictionary<string,string>
+            {
+                {"token",  settings.Token}
+            };
+            string url = settings.Url + "/attereco/api/v1/users/" + user.Sid + "/attend_sid";
+            Task task = Task.Factory.StartNew(
+                async () =>
+                {
+                    var content = new FormUrlEncodedContent(form);
+                    var response = await httpClient.PostAsync(url, content);
+                    Console.WriteLine(response.ToString());
+                }
+            );
+            try
+            {
+                task.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Source);
+            }
+            return user;
         }
     }
 }
