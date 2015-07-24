edad2578bb0fe500d3218e54292273f5f9362b51
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-24 23:57:47 +0900

    [add] refs #5 XML読み込み用のクラスを追加

---
+++ Attereco-Front/Model/Common/XMLFileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Attereco_Front.Model.Common
+{
+    public static class XMLFileManager
+    {
+        //--- 定数
+
+        private static readonly string Path = System.AppDomain.CurrentDomain.BaseDirectory;
+
+        //--- フィールド
+
+        //--- private メソッド
+
+        //--- static メソッド
+
+        public static T ReadXml<T>(string fileName) where T : new()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StreamReader stream;
+            T obj;
+            try
+            {
+                stream = new StreamReader(Path + fileName);
+                obj = (T)serializer.Deserialize(stream);
+                stream.Close();
+            }
+            catch (System.Exception)
+            {
+                obj = new T();
+            }
+            return obj;
+        }
+
+        public static void WriteXml<T>(string fileName, T obj)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StreamWriter stream = new StreamWriter(Path + fileName);
+            System.Console.WriteLine(Path + fileName);
+            serializer.Serialize(stream, obj);
+            stream.Close();
+        }
+    }
+}
