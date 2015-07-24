a8c0abd8c75dc078776415611a790eb364aee1a4
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-24 23:58:16 +0900

    [add] refs #5 Clientの設定用のクラスを追加する

---
+++ Attereco-Front/Model/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attereco_Front.Model
+{
+    public class Settings
+    {
+        public string Url { get; set; }
+
+        public string Token { get; set; }
+    }
+}
